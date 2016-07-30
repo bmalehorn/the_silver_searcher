3684b21de09b07781d73afdec9732a284f012cef
Brian Malehorn <bmalehorn@gmail.com>
2016-07-29 21:54:50 -0700

    add mem_capacity, set constants:
    mem_capacity = 2MB
    blocks_capacity = 100
    
    for i in $(seq 20); do MEM=$((i*500*1024)) CAPACITY=1000 /usr/bin/time -f "$((i*500))K	%e	%M" ag bmalehorn; done 2>&1 | grep -v ommand
    500K	1.06	5728
    1000K	0.99	6172
    1500K	0.98	6496
    2000K	0.96	6944
    2500K	0.98	7788
    3000K	0.93	7860
    3500K	0.97	8640
    4000K	0.94	8876
    4500K	0.97	9384
    5000K	0.94	10980
    5500K	0.94	10388
    6000K	0.94	10884
    6500K	0.95	11492
    7000K	0.95	11952
    7500K	1.00	12472
    8000K	0.94	12840
    8500K	0.96	13380
    9000K	0.94	14512
    9500K	0.95	14320
    10000K	0.94	14824
    
    $ for i in $(seq 20); do MEM=$((2*1024*1024)) CAPACITY=$((i*100)) /usr/bin/time -f "$((i*100))	%e	%M" ag bmalehorn; done 2>&1 | grep -v ommand
    100	1.01	7232
    200	0.95	7088
    300	0.99	7160
    400	0.95	7004
    500	0.97	7148
    600	0.98	7136
    700	0.96	7080
    800	0.96	7068
    900	0.99	7012
    1000	0.94	7072
    1100	0.94	7024
    1200	0.94	7064
    1300	0.94	7060
    1400	0.98	7052
    1500	0.96	7064
    1600	0.98	7068
    1700	0.98	7160
    1800	0.96	7180
    1900	0.98	7060
    2000	0.95	7068

--- src/mmap.c
+++ src/mmap.c
@@ -26,6 +26,8 @@ typedef struct {
 static mmap_block_t *blocks;
 static int blocks_capacity;
 static int blocks_n;
+static int mem_capacity;
+static int mem_n;
 
 
 void init_mmap(void) {
@@ -33,11 +35,16 @@ void init_mmap(void) {
     if (pthread_mutex_init(&mmap_mtx, NULL)) {
         die("Could not initialize mmap_mtx!");
     }
+    char *mem = getenv("MEM");
+    if (!mem || !(mem_capacity = atoi(mem))) {
+        mem_capacity = 2 * 1024 * 1024;
+    }
     char *cap = getenv("CAPACITY");
     if (!cap || !(blocks_capacity = atoi(cap))) {
-        blocks_capacity = 10;
+        blocks_capacity = 100;
     }
     blocks_n = 0;
+    mem_n = 0;
     blocks = ag_malloc(sizeof(*blocks) * blocks_capacity);
 }
 
@@ -63,17 +70,21 @@ void ag_munmap(void *addr, off_t f_len) {
     mmap_block_t *old_blocks = NULL;
     int i;
     int n;
+    int old_n = 0;
 
     pthread_mutex_lock(&mmap_mtx);
 
     n = blocks_n++;
     blocks[n].buf = addr;
     blocks[n].len = f_len;
+    mem_n += round_up_to_pagesize(f_len);
 
-    if (blocks_n == blocks_capacity) {
+    if (blocks_n == blocks_capacity || mem_n >= mem_capacity) {
         old_blocks = blocks;
+        old_n = blocks_n;
         blocks = ag_malloc(sizeof(*blocks) * blocks_capacity);
         blocks_n = 0;
+        mem_n = 0;
     }
 
     pthread_mutex_unlock(&mmap_mtx);
@@ -82,11 +93,12 @@ void ag_munmap(void *addr, off_t f_len) {
         char *base = NULL;
         off_t total_len = 0;
 
-        qsort(old_blocks, blocks_capacity, sizeof(*old_blocks), block_cmp);
+        qsort(old_blocks, old_n, sizeof(mmap_block_t), block_cmp);
 
-        for (i = 0; i < blocks_capacity; i++) {
+        for (i = 0; i < old_n; i++) {
             void *buf = old_blocks[i].buf;
             off_t len = round_up_to_pagesize(old_blocks[i].len);
+            log_debug("munmap %p, 0x%05zx", buf, len);
             if (base == NULL) {
                 base = buf;
                 total_len = len;
@@ -98,6 +110,8 @@ void ag_munmap(void *addr, off_t f_len) {
                 total_len = len;
             }
         }
+        log_debug("munmap");
+
 
         do_munmap(base, total_len);
 
